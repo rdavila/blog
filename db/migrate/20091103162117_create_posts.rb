ef2ad8757c500353772dbd0fa6000ac02ba23cbc
Ruben Davila <rdavila84@gmail.com>
2009-11-03 11:38:29 -0500

    relaciones entre post y categoria

--- db/migrate/20091103162117_create_posts.rb
+++ db/migrate/20091103162117_create_posts.rb
@@ -1,10 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def self.up
     create_table :posts do |t|
-      t.string    :title, :slug
-      t.boolean   :active
-      t.text      :body, :body_html, :excerpt, :excerpt_html
-      t.datetime  :published_at
+      t.references  :category
+      t.string      :title, :slug
+      t.boolean     :active
+      t.text        :body, :body_html, :excerpt, :excerpt_html
+      t.datetime    :published_at
       t.timestamps
     end
   end
