485a8d301e2a643ddd08376196266fb7d9bbef02
Ruben Davila <rdavila84@gmail.com>
2009-11-08 15:28:18 -0500

    corrigiendo orden para Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  default_scope :order => :published_at, :conditions => { :active => true }
+  default_scope :order => 'published_at DESC', :conditions => { :active => true }
 
   belongs_to :category, :counter_cache => true
 end
