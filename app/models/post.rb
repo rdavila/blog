2f2ccba9c05e3f1bebc1388507cc91cd785547d1
rdavila <rdavila84g@gmail.com>
2009-11-12 16:41:03 -0500

    validaciones para Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   default_scope :order => 'published_at DESC', :conditions => { :active => true }
 
+  validates_presence_of :title, :slug, :body, :published_at
+
   has_many :categorizations, :dependent => :delete_all
   has_many :categories, :through => :categorizations
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -2,4 +2,7 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   should_have_many :categories, :through => :categorizations
+
+  should_validate_presence_of :title, :slug, :body, :published_at
+  #show: id, title, slug, active, body, excerpt, published_at, created_at, updated_at
 end
