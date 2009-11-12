3012e89ac0456bc4bf51b914830725da7ede34fa
rdavila <rdavila84g@gmail.com>
2009-11-12 16:43:02 -0500

    validaciones para Category, config en Typus

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
+  validates_presence_of :name
+
   has_many :categorizations
   has_many :posts, :through => :categorizations
 end
--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
+  should_validate_presence_of :name
+
   should_have_many :categorizations
   should_have_many :posts, :through => :categorizations
 end
