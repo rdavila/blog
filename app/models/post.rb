555f134f1a0ff2e49146f0898131528f2ba49e5c
Ruben Davila <rdavila84@gmail.com>
2009-11-15 10:25:16 -0500

    preguardando post formateado

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ActiveRecord::Base
   default_scope :order => 'published_at DESC', :conditions => { :active => true }
 
+  before_save :generate_html
+
   validates_presence_of :title, :slug, :body, :published_at
 
   has_many :categorizations, :dependent => :delete_all
   has_many :categories, :through => :categorizations
+
+  protected
+
+    def generate_html
+      self.body_html = PostFormatter.format(self.body) if self.valid?
+    end
 end
---
+++ lib/post_formatter.rb
@@ -0,0 +1,26 @@
+class PostFormatter
+  extend TmSyntaxHighlighting::Helper
+  extend Haml::Helpers
+
+  def self.format(text)
+    text_pieces = text.split(/(<code>|<code lang="[A-Za-z0-9_-]+">|<code lang='[A-Za-z0-9_-]+'>|<\/code>)/)
+    in_pre = false
+    language = nil
+    text_pieces.collect do |piece|
+      if piece =~ /^<code( lang=(["'])?(.*)\2)?>$/
+        language = $3
+        in_pre = true
+        nil
+      elsif piece == "</code>"
+        in_pre = false
+        language = nil
+        nil
+      elsif in_pre
+        lang = language ? language : "ruby"
+        preserve(code(piece.strip, :lang => lang))
+      else
+        textilize(piece)
+      end
+    end.to_s
+  end
+end
