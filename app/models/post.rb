30d0e11579fa3df91485b298cd51328b10f69cfb
Ruben Davila <rdavila84@gmail.com>
2009-11-15 12:44:05 -0500

    implementando frieldy_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ActiveRecord::Base
-  default_scope :order => 'published_at DESC', :conditions => { :active => true }
+  has_friendly_id :title, :use_slug => true
+
+  default_scope :order => 'published_at DESC', :conditions => { 'posts.active' => true }
 
   before_save :generate_html
 
-  validates_presence_of :title, :slug, :body, :published_at
+  validates_presence_of :title, :body, :published_at
 
   has_many :categorizations, :dependent => :delete_all
   has_many :categories, :through => :categorizations
