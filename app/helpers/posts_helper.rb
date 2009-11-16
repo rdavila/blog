d590382b9cce4616a890a5a8b39c4daf577c0de1
rdavila <rdavila84g@gmail.com>
2009-11-16 16:11:26 -0500

    integracion con disqus

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
-  def post_link(post)
-    link_to post.title, 
-      seo_post_path(post.published_at.year, post.published_at.month, post.published_at.day, post.friendly_id)
+  def post_link(post, title=nil, anchor=nil)
+    link_to title || post.title, 
+      seo_post_url(post.published_at.year, post.published_at.month, post.published_at.day, post.friendly_id, :anchor => anchor)
   end
 end
