8ae26ae352773ec7fdf855b76bb95d329c7a3ed2
rdavila <rdavila84g@gmail.com>
2009-11-18 10:13:54 -0500

    controlador para paginas estaticas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ ActionController::Routing::Routes.draw do |map|
   map.root      :controller => 'homes', :action => 'show'
 
   map.resource  :ui
+  map.resources :posts, :only => :show
+
   map.seo_post '/:year/:month/:day/:id', 
     :controller => 'posts', :action => 'show'
+
+  map.connect ':action', :controller => 'pages'
 end
