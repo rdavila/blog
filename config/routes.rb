468fdfe18c630c5561f9a42b6a191db8ee1951ac
rdavila <rdavila84g@gmail.com>
2009-12-01 16:38:03 -0500

    agregando rutas por defecto para que trabaje typus

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :posts, :only => :show
   map.resources :categories
 
+  map.connect '/search', :controller => 'posts', :action => 'search'
+
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format' 
+  map.connect ':action', :controller => 'pages'
   map.seo_post '/:year/:month/:day/:id', 
     :controller => 'posts', :action => 'show'
-
-  map.connect '/search', :controller => 'posts', :action => 'search'
-  map.connect ':action', :controller => 'pages'
 end
